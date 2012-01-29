14c3e4382d9417ae1eeffcce58917c307ea69703
Bing Han <h.bing612@gmail.com>
2012-01-29 21:10:04 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApexSite::Application.config.secret_token = 'a20c7712d70d8765ae174e83a4a289ff8d7f66b8d72c9d85408016ab40068b283d7c0efc1e074f2fa3c74490abdcef1671dc9f5b4915d39ba88969228f9906da'
